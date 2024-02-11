74db9a395f2b1540f2018c4653bc95a79ffd0fa9
Nikolai <107766413+demonzloebychi@users.noreply.github.com>
2024-02-12 02:06:48 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -171,6 +171,58 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
 
   document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '#commemts' );
+    var splide = new Splide( '#commemts', {
+      type   : 'loop',
+      perPage: 3,
+      start  : 1,
+      padding: 30,
+      classes: {
+        pagination: 'splide__pagination your-class-pagination',
+        page      : 'splide__pagination__page your-class-page',
+      },
+      breakpoints : {
+        1200: {
+          perPage: 2,
+        },
+        768: {
+          perPage: 1,
+        }
+      },
+
+    } );
     splide.mount();
   } );
+
+
+
+  let date = new Date('Feb 13 2024 00:00:00');
+
+  function counts(){
+    let now = new Date();
+    gap = date - now;
+
+
+    let hours = Math.floor(gap / 1000 / 60 / 60);
+    let minutes = Math.floor(gap / 1000 / 60 ) % 60;
+    let seconds = Math.floor(gap / 1000 ) % 60;
+    
+
+    if(minutes < 10){
+      minutes = '0'+ minutes;
+    }
+    if(seconds < 10){
+      seconds = '0'+ seconds;
+    }
+
+    if(gap<0){
+      document.getElementById('group').innerText = 'ALREADY';
+    }else{
+      document.getElementById('hours').innerText = hours + ' :';
+      document.getElementById('minutes').innerText = minutes + ' :';
+      document.getElementById('seconds').innerText = seconds;
+    }
+
+  };
+  counts()
+
+  setInterval(counts, 1000);
